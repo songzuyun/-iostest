67fab4d33ac44334f01f495a9b4df603d7a94c03
凯恩斯 <keynes_ios@kaiensideMacBook-Air.local>
2017-11-22 14:26:25 +0800

    first

---
+++ kens/gittest/gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by 凯恩斯 on 2017/11/22.
+//  Copyright © 2017年 凯恩斯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kens/gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by 凯恩斯 on 2017/11/22.
+//  Copyright © 2017年 凯恩斯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
